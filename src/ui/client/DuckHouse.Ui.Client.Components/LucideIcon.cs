051657fbc98e7ec6495d92712667b2544db7c1f0
Tommi Hirvonen <tommih_@hotmail.com>
2026-04-08 07:55:34 +0300

    removed custom icons and related source generation

--- src/ui/client/DuckHouse.Ui.Client.Components/LucideIcon.cs
+++
@@ -1,6 +0,0 @@
-using DuckHouse.Ui.Client.Icons;
-
-namespace DuckHouse.Ui.Client.Components;
-
-[GenerateIcons(cssClass: "lucide", "Icons", "lucide")]
-public static partial class LucideIcon;
--- src/ui/client/DuckHouse.Ui.Client.Icons/Svg.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DuckHouse.Ui.Client.Icons;
-
-public class Svg(string svg)
-{
-    public string Text { get; } = svg; 
-}
